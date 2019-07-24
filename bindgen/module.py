02d8a0aab15d2dada35ee4210eaa03917134c5c4
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2019-07-24 23:25:16 +0200

    Implement class sorting

--- bindgen/module.py
+++ bindgen/module.py
@@ -49,6 +49,32 @@ class ModuleInfo(object):
             self.operators.extend(h.operators)
             self.class_dict.update(h.class_dict)
             
+        self.sort_classes()
+            
+    def sort_classes(self):
+        
+        class_dict = {c.name : c for c in self.classes}
+        classes_old = list(self.classes)
+        classes = []
+        
+        # put all rootclasses at the top
+        for c in classes_old:
+            print(c.rootclass)
+            root = class_dict.get(c.rootclass,None)
+            if root in classes_old:
+                classes.append(root)
+                classes_old.remove(root)
+        
+        while classes_old:
+            #import pdb; pdb.set_trace()
+            for c in classes_old:
+                superclass = class_dict.get(c.superclass,None)
+                if superclass in classes or superclass not in self.classes:
+                    classes.append(c)
+                    classes_old.remove(c)
+                    
+        self.classes = classes
+            
 if __name__ == '__main__':
     
     from os import getenv
