eb10739662901705f858dcb1a17c587f7cf7cac0
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2019-08-02 11:58:56 +0200

    Use cymbal to monkeypatch libclang

--- bindgen/utils.py
+++ bindgen/utils.py
@@ -1,4 +1,6 @@
-from clang.cindex import Config, Index
+from clang.cindex import Config, Index, Cursor
+from cymbal import monkeypatch_cursor
+from ctypes import c_uint
 from path import Path
 from os import getenv
 
@@ -13,8 +15,31 @@ def init_clang():
         conda_prefix = Path(getenv('CONDA_PREFIX'))
         Config.set_library_file(conda_prefix / 'lib' / 'libclang.so')
         
+        # Monkeypatch clang
+        monkeypatch_cursor('is_virtual',
+                           'clang_isVirtualBase',
+                           [Cursor], c_uint)
+        
+        monkeypatch_cursor('is_inline',
+                           'clang_Cursor_isFunctionInlined',
+                           [Cursor], c_uint)
+        
+        
+        monkeypatch_cursor('get_specialization',
+                           'clang_getSpecializedCursorTemplate',
+                           [Cursor], Cursor)
+        
+        monkeypatch_cursor('get_template_kind',
+                           'clang_getTemplateCursorKind',
+                           [Cursor], c_uint)
+        
+        monkeypatch_cursor('get_num_overloaded_decl',
+                           'clang_getNumOverloadedDecls',
+                           [Cursor], c_uint)
+        
         initialized = True
         ix = Index.create()
+
         
 def get_index():
     
