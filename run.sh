d3f9bfc92b302ecbf9d04b3e2ca9c864fb1d8cc3
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2019-08-04 21:15:36 +0200

    3-stage run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python -m bindgen -n 7 -v parse gp.toml out.pkl && python -m bindgen -n 7 -v generate gp.toml out.pkl
+python -m bindgen -n 7 -v parse gp.toml out.pkl && python -m bindgen -n 7 -v transform gp.toml out.pkl out_f.pkl && python -m bindgen -n 7 -v generate gp.toml out_f.pkl
