301374a464cc4b5a9b55f3bc011dfc339efa15c0
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2019-12-10 21:01:38 +0100

    More logical name for the conf file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python -m bindgen -n 7 -v parse gp.toml out.pkl && python -m bindgen -n 7 -v transform gp.toml out.pkl out_f.pkl && python -m bindgen -n 7 -v generate gp.toml out_f.pkl
+python -m bindgen -n 7 -v parse ocp.toml out.pkl && python -m bindgen -n 7 -v transform ocp.toml out.pkl out_f.pkl && python -m bindgen -n 7 -v generate ocp.toml out_f.pkl
