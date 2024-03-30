0f9facdaad9e40ae390bba87375b979c258dd731
efferre79 <rossi.f@inwind.it>
2024-03-30 14:31:57 +0100

    add a comment on expected version string

--- bindgen/utils.py
+++ bindgen/utils.py
@@ -56,6 +56,7 @@ def get_clang_version() :
 
     if not initialized: init_clang()
 
+    # clangVersion should be a string like "clang version MAJOR.MINOR.PATCH"
     return int(clangVersion.split()[-1].split(".")[0])
 
 
